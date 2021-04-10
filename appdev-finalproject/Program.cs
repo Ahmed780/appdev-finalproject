78766e0651a51fe1f0b706e0f53137d92fffe148
Cry0 <cryogen65@gmail.com>
2021-04-10 17:14:34 -0400

    Finished implementation of GUI & added CSV file parser ref +logo

--- appdev-finalproject/Program.cs
+++ appdev-finalproject/Program.cs
@@ -16,7 +16,7 @@ namespace appdev_finalproject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new loginForm());
         }
     }
 }
